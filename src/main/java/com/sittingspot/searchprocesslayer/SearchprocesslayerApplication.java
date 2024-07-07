34481143f9d98b11445c8e59eaf98a0526e46405
Leonardo Malcotti <leo@Leonardos-Air.lan>
2024-07-07 14:30:53 +0200

    initialized spring app

---
+++ src/main/java/com/sittingspot/searchprocesslayer/SearchprocesslayerApplication.java
@@ -0,0 +1,13 @@
+package com.sittingspot.searchprocesslayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchprocesslayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchprocesslayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sittingspot/searchprocesslayer/SearchprocesslayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sittingspot.searchprocesslayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchprocesslayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
